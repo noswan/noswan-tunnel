71793cbe2ba45f4766ec4246ec34032fcf6e7308
Ya Zhuang <zhuangya@gmail.com>
2016-06-06 01:26:24 +0800

    fix: get correct ip

--- lib.js
+++ lib.js
@@ -2,13 +2,14 @@
 
 const url = require('url');
 
+const ip = require('ip');
 const qr = require('qrcode-terminal');
 const forward = require('http-port-forward');
 
 module.exports = (dest, port) => {
   const parsed = url.parse(dest);
   const from = parsed.port;
-  const f = Object.assign({}, parsed, { port: port, host: null });
+  const f = Object.assign({}, parsed, { hostname: ip.address(), port: port, host: null });
   forward(from, port);
   qr.generate(url.format(f));
 };
